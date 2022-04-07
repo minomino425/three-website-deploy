fa06fe647a90551d35983cefd5b794e4e5ebc2d5
minomino425 <100152875+minomino425@users.noreply.github.com>
2022-04-07 09:15:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const canvas = document.querySelector("#webgl");
 const scene = new THREE.Scene();
 //背景用のﾃｷｽﾁｬ
 const textureLoader = new THREE.TextureLoader();
-const bgTexture = textureLoader.load("/three-scroll-website.vercel.app/bg/bg.jpg");
+const bgTexture = textureLoader.load("/bg/bg.jpg");
 scene.background = bgTexture;
 
 //オブジェクト
